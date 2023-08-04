04482da1968fb4178b58edeeec1cb12f7a2933e6
any santos <anyy.gabriele@gmail.com>
2023-08-04 17:12:38 -0400

    commit inicial

--- migrations/20230804193100-create-usuario.js
+++ migrations/20230804193100-create-usuario.js
@@ -14,12 +14,14 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+    },
+    updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+    }
     });
   },
   async down(queryInterface, Sequelize) {
